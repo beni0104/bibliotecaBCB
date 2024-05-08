dc2b0b2d4a4437c9c35f279ac447d6aac26c6556
beni <szekrenyes.beni@gmail.com>
2024-05-08 18:40:24 +0300

    added internationalization (en,ro) + made some changes to the UI

--- frontendBibliotecaBCB/frontendBibliotecaBCB/src/app/layout/Loan Requests/loan-requests-page/loan-requests-page.component.ts
+++ frontendBibliotecaBCB/frontendBibliotecaBCB/src/app/layout/Loan Requests/loan-requests-page/loan-requests-page.component.ts
@@ -9,6 +9,13 @@ import { LoanService } from '../../../services/loan.service';
 })
 export class LoanRequestsPageComponent {
   loanRequests: LoanRequest[] = [];
+  displayedLoanRequests: LoanRequest[] = [];
+  filteredLoanRequests: LoanRequest[] = [];
+
+  searchTerm: string = '';
+  acceptedSelected: boolean = false;
+  deniedSelected: boolean = false;
+  processingSelected: boolean = false;
 
   constructor(private loanService: LoanService) {
   }
@@ -16,6 +23,68 @@ export class LoanRequestsPageComponent {
   ngOnInit() {
     this.loanService.getLoanRequests().then((loanRequests: any) => {
       this.loanRequests = loanRequests;
+      this.displayedLoanRequests = loanRequests;
     });
+  }
+
+  sortLoanRequests(type: string){
+    if (type === 'pickup-date') {
+      this.displayedLoanRequests.sort((a, b) => {
+        const dateA = new Date(a.pickupDate);
+        const dateB = new Date(b.pickupDate);
+        return dateA.getTime() - dateB.getTime();
+      });
+    } else if (type === 'requested-date') {
+      this.displayedLoanRequests.sort((a, b) => {
+        const dateA = new Date(a.requestedDate);
+        const dateB = new Date(b.requestedDate);
+        return dateA.getTime() - dateB.getTime();
+      });
+    }
+  }
+
+  clearFilters(){
+    this.acceptedSelected = false;
+    this.deniedSelected = false;
+    this.processingSelected = false;
+    this.displayedLoanRequests = this.loanRequests;
+  }
+
+  applyFilters(){
+    if(!this.acceptedSelected && !this.deniedSelected && !this.processingSelected){
+      this.displayedLoanRequests = this.loanRequests;
+    } else {
+      this.displayedLoanRequests = this.loanRequests.filter((loanRequest: LoanRequest) => {
+        if (this.acceptedSelected && loanRequest.status === 'ACCEPTED') {
+          return true;
+        }
+        if (this.deniedSelected && loanRequest.status === 'DENIED') {
+          return true;
+        }
+        if (this.processingSelected && loanRequest.status === 'PROCESSING') {
+          return true;
+        }
+        return false;
+      });
+    }
+  }
+
+  search(){
+    if (this.searchTerm.trim() === '') {
+      this.displayedLoanRequests = this.loanRequests;
+    } else {
+      this.displayedLoanRequests = this.loanRequests.filter((loanRequest: LoanRequest) => {
+        const searchTermLower = this.searchTerm.toLowerCase();
+        const bookTitleLower = (loanRequest.bookTitle ?? '').toLowerCase();
+        const bookAuthorLower = (loanRequest.bookAuthor ?? '').toLowerCase();
+        const userNameLower = (loanRequest.userName ?? '').toLowerCase();
+
+        return (
+          bookTitleLower.includes(searchTermLower) ||
+          bookAuthorLower.includes(searchTermLower) ||
+          userNameLower.includes(searchTermLower)
+        );
+      });
+    }
   }
 }
--- frontendBibliotecaBCB/frontendBibliotecaBCB/src/app/layout/Manage Loans/loan-page/loan-page.component.ts
+++ frontendBibliotecaBCB/frontendBibliotecaBCB/src/app/layout/Manage Loans/loan-page/loan-page.component.ts
@@ -9,13 +9,24 @@ import { LoanService } from '../../../services/loan.service';
 })
 export class LoanPageComponent {
   userList: UsernameAndId[] = [];
+  displayedUserList: UsernameAndId[] = [];
+  searchTerm: string = '';
 
   constructor(private loanService: LoanService) {}
 
   ngOnInit(): void {
     this.loanService.getUsernamesAndIds().then((users: any) => {
       this.userList = users;
+      this.displayedUserList = users;
     });
+  }
+
+  search() {
+    if (this.searchTerm.trim() === '') {
+      this.displayedUserList = this.userList;
+    } else {
+      this.displayedUserList = this.userList.filter(user => user.name.toLowerCase().includes(this.searchTerm.toLowerCase()));
+    }
   }
 
 }
