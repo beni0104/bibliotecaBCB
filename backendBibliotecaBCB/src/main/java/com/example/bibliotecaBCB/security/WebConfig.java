d21888961d183f28e86ea22a91762591b2306564
beni <szekrenyes.beni@gmail.com>
2024-10-08 15:12:47 +0300

    some changes to environment variables

--- backendBibliotecaBCB/src/main/java/com/example/bibliotecaBCB/security/WebConfig.java
+++ backendBibliotecaBCB/src/main/java/com/example/bibliotecaBCB/security/WebConfig.java
@@ -8,12 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${LOCALHOST}")
+    @Value("${MY_APP_IP}")
     private String corsOrigin;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**").allowedOrigins(corsOrigin)
+        registry.addMapping("/api/**").allowedOrigins("https://" + corsOrigin + "4200/")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
     }
 }
