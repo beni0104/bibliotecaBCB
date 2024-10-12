e2565b5bf5126d8331b28299e4337084a31223be
beni <szekrenyes.beni@gmail.com>
2024-10-12 13:57:52 +0300

    configured something related to internationalization

--- frontendBibliotecaBCB/frontendBibliotecaBCB/src/app/app.module.ts
+++ frontendBibliotecaBCB/frontendBibliotecaBCB/src/app/app.module.ts
@@ -40,7 +40,7 @@ const icons = {
 };
 
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, `${environment.apiUrl}/assets/i18n/`, '.json');
+  return new TranslateHttpLoader(http, `./assets/i18n/`, '.json');
 }
 
 @NgModule({
